aba3921bf967d2596673e0d5fe0ec1201fcdad07
boots-ms <muddshells@gmail.com>
2026-02-25 12:20:21 -0800

    background stuff

--- Climbing_Project/Assets/Scripts/MusicAndBackground.cs
+++ Climbing_Project/Assets/Scripts/MusicAndBackground.cs
@@ -7,15 +7,43 @@ public class MusicAndBackground : MonoBehaviour
     public float level2Threshold;
     Transform player;
 
+    public SpriteRenderer sr;
+    public Sprite[] levelBackgrounds;
+
+    float levelProgressRatio;
+    Transform victory;
+
+    public float textureHeight;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        sr = GetComponent<SpriteRenderer>();
+        victory = GameObject.FindGameObjectWithTag("Victory").transform;
     }
 
     private void Update()
     {
-        if (player.transform.position.y > level2Threshold)      level = 2;
-        else if (player.transform.position.y > level1Threshold) level = 1;
+        if (player.position.y > level2Threshold)      level = 2;
+        else if (player.position.y > level1Threshold) level = 1;
         else                                                    level = 0;
+    
+        sr.sprite = levelBackgrounds[level];
+
+        switch (level)
+        {
+            case 0:
+                levelProgressRatio = player.position.y / level1Threshold;
+                break;
+            case 1:
+                levelProgressRatio = (player.position.y - level1Threshold)/(level2Threshold - level1Threshold);
+                break;
+            case 2:
+                levelProgressRatio = (player.position.y - level2Threshold) / (victory.position.y - level2Threshold);
+                break;
+
+        }
+
+        transform.localPosition = new Vector2(0, -(textureHeight * levelProgressRatio - textureHeight / 2));
     }   
 }
